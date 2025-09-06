8f96c0ab350f6d235280bc2b43a0634fd2386f10
weiilinhan <298998024@qq.com>
2025-09-06 23:35:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-weight=input("请输入重量：")
+weight=input(＂请输入重量：＂)
 num=float(weight[:-2])
 uint=weight[-2:]
-if unit==“kg”:
+if unit==＂kg＂:
     pound=round(num*2.2046,3)
-    print(f"对应的英制重量为{pound}磅")
-elif unit==“pd”:
+    print(f＂对应的英制重量为{pound}磅＂)
+elif unit==＂pd＂:
     kg =round(num*0.4535,3)
-    print(f"对应的公制重量为{kg}公斤")
+    print(f＂对应的公制重量为{kg}公斤＂)
 else:
-    print("输入格式错误")
+    print(＂输入格式错误＂)
   
