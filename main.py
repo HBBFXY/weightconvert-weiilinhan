19c295c6336bce30255e3e86f3784f98490b2c1e
weiilinhan <298998024@qq.com>
2025-09-06 23:25:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if unit==＇kg＇:
     pound=round(num*2.2046,3)
     print(f"对应的英制重量为{pound}磅")
 elif unit==＇pd＇:
-    kg =round(num*0.4535,)
+    kg =round(num*0.4535,3)
     print(f"对应的公制重量为{kg}公斤")
 else:
     print("输入格式错误")
